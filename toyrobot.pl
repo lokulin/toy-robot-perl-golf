04d9815a35e27fa66f9673d22d6956594c3103bc
lokulin <lauchlin@lauchlin.com>
2015-06-01 19:24:49 +1000

    Removed unneeded returns and mys

--- toyrobot.pl
+++ toyrobot.pl
@@ -1,11 +1,7 @@
 #!/usr/bin/env perl
-use Math::Trig;use POSIX qw(floor);use List::MoreUtils qw(firstidx);
-my @d=('NORTH','EAST','SOUTH','WEST');my $p="(".join("|",@d).")";
-my @r=([0,0],0,0);
-my $m=sub {return $_[0]>=0 && $_[1]>=0 && $_[0]<=4 && $_[1]<=4};
-sub a{return ($_[0][0]+sin($_[1]*pi),$_[0][1]+cos($_[1]*pi))};
-sub f{return $_[0]-2.0*floor($_[0]/2.0)};
-while(<>){chomp;$_=~/MOVE/&&$m->(a(@r))?$r[0]=[a(@r)]:
-$_=~/LEFT/?$r[1]=f($r[1]-0.5):$_=~/RIGHT/?$r[1]=f($r[1]+0.5):
-$_=~/^PLACE\s(\d+),(\d+),$p$/&&$m->($1,$2)?@r=([$1,$2],(firstidx{/$3/}@d)/2,1):
-$_=~/REPORT/&&$r[2]?print($r[0][0].",".$r[0][1].",".$d[int($r[1]*2)]."\n"):_;}
+use Math::Trig;use List::MoreUtils ':all';@d=qw(NORTH EAST SOUTH WEST);
+$p="(".join("|",@d).")";@r=([0,0],0,0);$m=sub{($x,$y)=@_;$x>=0&&$y>=0&&$x<=4
+&&$y<=4};sub a{($_[0][0]+sin($_[1]*pi/2),$_[0][1]+cos($_[1]*pi/2))};while(<>)
+{chomp;/MOVE/&&$m->(a(@r))?$r[0]=[a(@r)]:/LEFT/?$r[1]--:/RIGHT/?$r[1]++:
+/PLACE\s(\d+),(\d+),$p/&&$m->($1,$2)?@r=([$1,$2],(firstidx{/$3/}@d),1):
+/REPORT/&&$r[2]?print($r[0][0].",".$r[0][1].",".$d[int($r[1]%4)]."\n"):_;}
