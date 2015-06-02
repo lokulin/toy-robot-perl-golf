716707cff82630b3ee423d6584266057c9169abe
lokulin <lauchlin@lauchlin.com>
2015-06-02 20:15:17 +1000

    improve matches and remove unneeded int cast

--- toyrobot.pl
+++ toyrobot.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -l ## valid commands: MOVE,LEFT,RIGHT,REPORT,PLACE x,y,facing ##
-use Math::Trig;@d=qw(NORTH EAST SOUTH WEST);$p=join("|",@d);$m=sub{($x,$y)=@_;$x
+use Math::Trig;@d=qw(NORTH EAST SOUTH WEST);$p=join("|",@d);sub t{($x,$y)=@_;$x
 >=0&&$y>=0&&$x<5&&$y<5};sub i{1while$_[0]ne pop;$#_};@r=([0,0],0,0);sub a{($_[0]
-[0]+sin($_[1]*pi/2),$_[0][1]+cos($_[1]*pi/2))};while(<>){/MOVE/&&$m->(a@r)?$r[0]
-=[a@r]:/LEFT/?$r[1]--:/RIGHT/?$r[1]++:/^PLACE (\d),(\d),($p)$/&&$m->($1,$2)?@r=(
-[$1,$2],i($3,@d),1):/REPORT/&&$r[2]?print"$r[0][0],$r[0][1],$d[int($r[1]%4)]":_}
+[0]+sin($_[1]*pi/2),$_[0][1]+cos($_[1]*pi/2))};while(<>){/^MOVE$/&&t(a@r)?$r[0]=
+[a@r]:/^LEFT$/?$r[1]--:/^RIGHT$/?$r[1]++:/^PLACE (\d),(\d),($p)$/&&t($1,$2)?@r=(
+[$1,$2],i($3,@d),1):/^REPORT$/&&$r[2]?print"$r[0][0],$r[0][1],$d[$r[1]%4]":_}
