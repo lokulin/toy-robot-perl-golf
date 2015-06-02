7b78580e3c307ad130fc05de21c33b16495eb85a
lokulin <lauchlin@lauchlin.com>
2015-06-02 23:00:01 +1000

    Conver def to lambda

--- toyrobot.rb
+++ toyrobot.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-include Math;d=%w{NORTH EAST SOUTH WEST};p=d.join("|");r=[[0,0],0];def t(x,y)x>=
-0&&y>=0&&x<5&&y<5 end;def a(x,y,z)[x+sin(z*PI/2),y+cos(z*PI/2)]end;ARGF.each{|l|
-l=~/^MOVE$/&&t(*a(*r[0],r[1]))?r[0]=a(*r[0],r[1]):l=~/^LEFT$/?r[1]-=1:l=~/^RIGHT
-$/x?r[1]+=1:l=~/^REPORT$/&&r[2]?puts("#{r[0][0]},#{r[0][1]},#{d[r[1]%4]}"):l=~/^
-PLACE\s(\d),(\d),(#{p})$/x&&t($1.to_i,$2.to_i)?r=[[$1.to_i,$2.to_i],d.index($3),1]:0}
+include Math;d=%w{NORTH EAST SOUTH WEST};p=d.join("|");t=->(x,y){x>=0&&y>=0&&
+x<5&&y<5};x,y,f,z=0,0,0,0;a=->{[x+sin(f*PI/2),y+cos(f*PI/2)]};$<.each{|l|l=~
+/^MOVE$/x&&t[*a[]]?(x,y=*a[]):l=~/^LEFT$/?f-=1:l=~/^RIGHT$/?f+=1:l=~/^REPORT$/&&
+z==1?puts("#{x.to_i},#{y.to_i},#{d[f%4]}"):l=~/^PLACE\s(\d),(\d),(#{p})$/&&t[$1
+.to_i,$2.to_i]?(x,y,f,z=$1.to_i,$2.to_i,d.index($3),1):0}
